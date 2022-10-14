5df36837cbe536f0702c3fd09850b170302b9e6b
Mickey Martinez <mickey@echobind.com>
2022-10-14 16:02:01 -0700

    dynamically map values to ImageOption

--- App.js
+++ App.js
@@ -10,24 +10,9 @@ export default function App() {
 
       <View style={styles.optionsContainer}>
         {/* dynamically map values from dummy data array */}
-        {/* {question.options.map(option => <ImageOption image={option.image} text={option.text}/> )} */}
-
-        <ImageOption
-          image={question.options[0].image}
-          text={question.options[0].text}
-        />
-        <ImageOption
-          image={question.options[1].image}
-          text={question.options[1].text}
-        />
-        <ImageOption
-          image={question.options[2].image}
-          text={question.options[2].text}
-        />
-        <ImageOption
-          image={question.options[3].image}
-          text={question.options[3].text}
-        />
+        {question.options.map((option) => (
+          <ImageOption image={option.image} text={option.text} />
+        ))}
       </View>
     </View>
   );
