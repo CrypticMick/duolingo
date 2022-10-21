bdd850cc03678fdab05a80634a7c7c5d3df2bf59
Mickey Martinez <mickey@echobind.com>
2022-10-21 15:52:50 -0700

    quiz logic: add currentQuestionIndex & useEffect

--- App.js
+++ App.js
@@ -1,25 +1,41 @@
-import { useState } from "react";
-import { Text, View } from "react-native";
+import { useState, useEffect } from "react";
+import { Text, View, Alert } from "react-native";
 import ImageOption from "./src/components/ImageOption";
 import Button from "./src/components/Button";
 
 import styles from "./App.styles";
-import question from "./assets/data/oneQuestionWithOption";
+import questions from "./assets/data/imageMultipleChoiceQuestions";
+// import question from "./assets/data/oneQuestionWithOption";
 
 export default function App() {
   const [selected, setSelected] = useState(null);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(
+    questions[currentQuestionIndex]
+  );
 
+  useEffect(() => {
+    console.log("useEffect called");
+    setCurrentQuestion(questions[currentQuestionIndex]);
+  }, [currentQuestionIndex]);
+
+  // check if selected choice is correct
   const onButtonPress = () => {
-    console.warn("Pressed");
+    if (selected.correct) {
+      Alert.alert("Correct!");
+      setCurrentQuestionIndex(currentQuestionIndex + 1);
+    } else {
+      Alert.alert("Wrong");
+    }
   };
 
   return (
     <View style={styles.root}>
-      <Text style={styles.title}>{question.question}</Text>
+      <Text style={styles.title}>{currentQuestion.question}</Text>
 
       <View style={styles.optionsContainer}>
         {/* dynamically map values from dummy data array */}
-        {question.options.map((option) => (
+        {currentQuestion.options.map((option) => (
           <ImageOption
             key={option.id}
             image={option.image}
